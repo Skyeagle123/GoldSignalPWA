df3e2ea10149c53899d5dc3b687addef86a2e9a2
Skyeagle123 <125151458+Skyeagle123@users.noreply.github.com>
2025-09-25 13:07:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ const HOURLY_CSV_URL = 'https://skyeagle123.github.io/GoldSignalPWA/XAUUSD_hourl
 
 // احتياطي سعر حي من Stooq (صف واحد: Symbol,Date,Time,Open,High,Low,Close,Volume)
 // بدّل تعريفات Stooq الحالية بهي النسخة عبر بروكسي يفتح CORS
+// استعمل بروكسي يفتح CORS
 const PROXY = 'https://r.jina.ai/http://';
-const STOOQ_LIVE_CSV   = PROXY + 'stooq.com/q/l/?s=xauusd&f=sd2t2ohlcv&h&e=csv'; // صف واحد حي
-const STOOQ_DAILY_OHLC = PROXY + 'stooq.com/q/d/l/?s=xauusd&i=d';                 // OHLC يومي لـ Pivot
+
+const STOOQ_LIVE_CSV   = PROXY + 'stooq.com/q/l/?s=xauusd&f=sd2t2ohlcv&h&e=csv';
+const STOOQ_DAILY_OHLC = PROXY + 'stooq.com/q/d/l/?s=xauusd&i=d';
 
 
 /************ DOM ************/
