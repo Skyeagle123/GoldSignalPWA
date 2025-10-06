715990e3902c3a04fd64a0f6c153703ec1085e82
Skyeagle123 <125151458+Skyeagle123@users.noreply.github.com>
2025-10-06 14:02:11 +0300

    Create sw1.js

---
+++ sw1.js
@@ -0,0 +1,12 @@
+
+self.addEventListener('install', (e)=>{
+  e.waitUntil(caches.open('gs-v1').then(cache=> cache.addAll([
+    './','./index.html','./app.js','./manifest.json','./XAUUSD_demo.csv',
+    './icons/icon-192.png','./icons/icon-512.png'
+  ])));
+});
+self.addEventListener('fetch', (e)=>{
+  e.respondWith(
+    caches.match(e.request).then(res=> res || fetch(e.request))
+  );
+});
