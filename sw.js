6f4a1016170a6c97bcbfeb94e6a849aab805789e
Skyeagle123 <125151458+Skyeagle123@users.noreply.github.com>
2025-10-07 14:27:40 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-// Disabled Service Worker temporarily to force live reload and bypass cache
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => self.clients.claim());
-self.addEventListener('fetch', (e) => {
-  // Always go to the network; no cache
-  e.respondWith(fetch(e.request));
-});
